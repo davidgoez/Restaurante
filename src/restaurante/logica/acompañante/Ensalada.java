be92952b69b9d75d5052d12544b3710d8eeedf15
damartinezru <danielamr15@hotmail.com>
2018-04-07 21:08:35 -0500

    subido

---
+++ src/restaurante/logica/acompañante/Ensalada.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package restaurante.logica.acompañante;
+
+/**
+ *
+ * @author GOLDS
+ */
+public class Ensalada extends Acompañante {
+    
+    public String zanahoria;
+    public String habichuela;
+    public String repollo;
+}
---
+++ src/restaurante/logica/bebida/Jugo.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package restaurante.logica.bebida;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class Jugo extends Bebida {
+    
+    public String mora;
+    public String mango;
+    public String lulo;
+    
+    
+}
